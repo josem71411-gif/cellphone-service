036eeb72cc8a77aa2dbdfd867f9d4b5edb71b61a
josem71411-gif <josem71411@gmail.com>
2026-04-15 13:11:11 -0500

    added the field variables, and also added teh default values for current objects

--- src/main/java/com/plurasight/CellPhone.java
+++ src/main/java/com/plurasight/CellPhone.java
@@ -1,4 +1,22 @@
 package com.plurasight;
 
 public class CellPhone {
+
+    //data members
+    private  int serialNumber;
+    private  String model;
+    private  String carrier;
+    private  String phoneNumber;
+    private  String owner;
+
+    // Parameterless constructor
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+
+    }
+    // Getters and setters
 }
